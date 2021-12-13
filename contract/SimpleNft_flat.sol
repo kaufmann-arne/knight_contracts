0bb0cb00e32f05eb7714a9093c157f72945dd65c
Mage Morgan <fortressdaofinance@gmail.com>
2021-12-13 12:59:30 +0100

    Add files via upload

--- contract/SimpleNft_flat.sol
+++ contract/SimpleNft_flat.sol
@@ -1,18 +1,5 @@
 // SPDX-License-Identifier: MIT
 
-// Amended by HashLips
-/**
-    !Disclaimer!
-    These contracts have been used to create tutorials,
-    and was created for the purpose to teach people
-    how to create smart contracts on the blockchain.
-    please review this code on your own before using any of
-    the following code for production.
-    HashLips will not be liable in any way if for the use 
-    of the code. That being said, the code has been tested 
-    to the best of the developers' knowledge to work as intended.
-*/
-
 // File: @openzeppelin/contracts/utils/introspection/IERC165.sol
 pragma solidity ^0.8.0;
 /**
@@ -1232,16 +1219,16 @@ abstract contract Ownable is Context {
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract NFT is ERC721Enumerable, Ownable {
+contract MyEyes is ERC721Enumerable, Ownable {
   using Strings for uint256;
 
   string baseURI;
   string public baseExtension = ".json";
-  uint256 public cost = 0.05 ether;
-  uint256 public maxSupply = 10000;
-  uint256 public maxMintAmount = 20;
-  bool public paused = false;
-  bool public revealed = false;
+  uint256 public cost = 1 ether;
+  uint256 public maxSupply = 500;
+  uint256 public maxMintAmount = 1;
+  bool public paused = true;
+  bool public revealed = true;
   string public notRevealedUri;
 
   constructor(
@@ -1311,7 +1298,7 @@ contract NFT is ERC721Enumerable, Ownable {
         : "";
   }
 
-  //only owner
+
   function reveal() public onlyOwner {
       revealed = true;
   }
@@ -1341,18 +1328,9 @@ contract NFT is ERC721Enumerable, Ownable {
   }
  
   function withdraw() public payable onlyOwner {
-    // This will pay HashLips 5% of the initial sale.
-    // You can remove this if you want, or keep it in to support HashLips and his channel.
-    // =============================================================================
-    (bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
-    require(hs);
-    // =============================================================================
-    
-    // This will payout the owner 95% of the contract balance.
-    // Do not remove this otherwise you will not be able to withdraw the funds.
-    // =============================================================================
+
     (bool os, ) = payable(owner()).call{value: address(this).balance}("");
     require(os);
-    // =============================================================================
+
   }
 }
